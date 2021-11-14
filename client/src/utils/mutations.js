1cbde2caf84fa52dd80284df5a50fb389694feb7
Topash15 <Alextopash@gmail.com>
2021-11-13 19:41:07 -0500

    added queries/mutations. login working

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client/";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation Mutation($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation Mutation($book: BookContent) {
+    saveBook(book: $book) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation Mutation($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        description
+        authors
+        title
+        image
+        link
+      }
+    }
+  }
+`;
--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const startServer = async () => {
 startServer();
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
